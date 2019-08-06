001c8d00692ac88e61c868b27cec19d052e82bb8
falcon <falcon@falcons-MacBook-Pro.local>
2019-08-06 14:56:43 +0300

    fix indentation

--- openpilot/selfdrive/car/mitsubishi/values.py
+++ openpilot/selfdrive/car/mitsubishi/values.py
@@ -11,16 +11,16 @@ class ECU:
 
 
 # addr: (ecu, cars, bus, 1/freq*100, vl)
- STATIC_MSGS = [
-   (0x128, ECU.DSU, (CAR.MIEV), 1,   3, '\xf4\x01\x90\x83\x00\x37'),
-   (0x141, ECU.DSU, (CAR.MIEV), 1,   2, '\x00\x00\x00\x46'),
-   (0x160, ECU.DSU, (CAR.MIEV), 1,   7, '\x00\x00\x08\x12\x01\x31\x9c\x51'),
-   (0x161, ECU.DSU, (CAR.MIEV), 1,   7, '\x00\x1e\x00\x00\x00\x80\x07'),
-   (0x344, ECU.DSU, (CAR.MIEV), 2,   5, '\x00\x00\x01\x00\x00\x00\x00\x50'),
-   (0x365, ECU.DSU, (CAR.MIEV), 2,  20, '\x00\x00\x00\x80\xfc\x00\x08'),
-   (0x366, ECU.DSU, (CAR.MIEV), 2,  20, '\x00\x72\x07\xff\x09\xfe\x00'),
-   (0x4CB, ECU.DSU, (CAR.MIEV), 2, 100, '\x0c\x00\x00\x00\x00\x00\x00\x00'),
- ]
+  STATIC_MSGS = [
+    (0x128, ECU.DSU, (CAR.MIEV), 1,   3, '\xf4\x01\x90\x83\x00\x37'),
+    (0x141, ECU.DSU, (CAR.MIEV), 1,   2, '\x00\x00\x00\x46'),
+    (0x160, ECU.DSU, (CAR.MIEV), 1,   7, '\x00\x00\x08\x12\x01\x31\x9c\x51'),
+    (0x161, ECU.DSU, (CAR.MIEV), 1,   7, '\x00\x1e\x00\x00\x00\x80\x07'),
+    (0x344, ECU.DSU, (CAR.MIEV), 2,   5, '\x00\x00\x01\x00\x00\x00\x00\x50'),
+    (0x365, ECU.DSU, (CAR.MIEV), 2,  20, '\x00\x00\x00\x80\xfc\x00\x08'),
+    (0x366, ECU.DSU, (CAR.MIEV), 2,  20, '\x00\x72\x07\xff\x09\xfe\x00'),
+    (0x4CB, ECU.DSU, (CAR.MIEV), 2, 100, '\x0c\x00\x00\x00\x00\x00\x00\x00'),
+  ]
 
  ECU_FINGERPRINT = {
    ECU.DSU: 0x343,   # accel cmd
